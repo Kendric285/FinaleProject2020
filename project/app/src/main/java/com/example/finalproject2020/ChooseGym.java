cf6c88d9b92cd23565fa91dd51ddb8bed2e08f45
Kendric Terry <kendric285@gmail.com>
2020-06-12 14:11:43 -0400

    Revert "h"
    This reverts commit 53cc54fd80236eb76f7da87a47b4895ea1a23c5d.

--- project/app/src/main/java/com/example/finalproject2020/ChooseGym.java
+++ project/app/src/main/java/com/example/finalproject2020/ChooseGym.java
@@ -2,37 +2,26 @@ package com.example.finalproject2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
 
 public class ChooseGym extends AppCompatActivity {
 
     ImageView frontArrow;
     ImageView backArrow;
     ImageView gymImageView;
-    ImageView userStarterPokemon;
 
     Integer gymNumber;
-
-    String userStarterPokemonImageURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_gym);
         gymImageView = findViewById(R.id.gymImageView);
-        userStarterPokemon = findViewById(R.id.userStarterPokemon);
 
         gymNumber = 1;
 
-        Intent getUserStarterPokemonImage = getIntent();
-        userStarterPokemonImageURL = getUserStarterPokemonImage.getStringExtra("starter");
-
-        Picasso.with(this).load(userStarterPokemonImageURL).into(userStarterPokemon);
 
 
 
