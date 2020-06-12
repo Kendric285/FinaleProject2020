9962df8685551ad7cf6548da91302678e92db3f9
Kendric Terry <kendric285@gmail.com>
2020-06-12 11:07:33 -0400

    pokemon
    Added starter pokemon

--- project/app/src/main/java/com/example/finalproject2020/MainActivity.java
+++ project/app/src/main/java/com/example/finalproject2020/MainActivity.java
@@ -16,7 +16,6 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,65 +25,52 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button add;
-    ImageView img;
+
+    ImageView bulbasaurImageView;
+    ImageView squirtleImageView;
+    ImageView charmanderImageView;
+
     Random r = new Random();
     int digiman = (r.nextInt(19) + 0);
-    String imgs;
+
+    String bulbasaurImageViewURL;
+    String squirtleImageViewURL;
+    String charmanderImageViewURL;
+
     TextView dad;
+
+
+    OkHttpClient client;
+
+    String bulbasaurApiInfoURL;
+    String squirtleApiInfoURL;
+    String charmanderApiInfoURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        client = new OkHttpClient();
+
         add = findViewById(R.id.add);
-        img = findViewById(R.id.imageView);
-        dad = findViewById(R.id.tv);
+
+        bulbasaurImageView = findViewById(R.id.bulbasaurImageView);
+        squirtleImageView = findViewById(R.id.squirtleImageView);
+        charmanderImageView = findViewById(R.id.charmanderImageView);
+
+        bulbasaurApiInfoURL = "https://pokeapi.co/api/v2/pokemon/1";
+        squirtleApiInfoURL = "https://pokeapi.co/api/v2/pokemon/7";
+        charmanderApiInfoURL = "https://pokeapi.co/api/v2/pokemon/4";
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
 
-                OkHttpClient client = new OkHttpClient();
 
-                String url = "https://pokeapi.co/api/v2/pokemon/"+digiman;
-                Log.d("mode", "onClick: ");
-                final Request request = new Request.Builder()
-                        .url(url)
-                        .get()
-                        .build();
+                starterPokemon();
 
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        e.printStackTrace();
-                        Log.d("mode", "onFailure: ");
-                    }
 
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        if (response.isSuccessful()) {
-                            final String myResponse = response.body().string();
-                           // Log.d("mode", "onResponse: " + myResponse);
-
-                            MainActivity.this.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                   try {
-                                        JSONObject obj = new JSONObject(myResponse);
-                                        //JSONArray info = obj.getJSONArray("sprites");
-                                       String name = obj.getString("name");
-                                        dad.setText(""+name);
-                                       JSONObject sprites = obj.getJSONObject("sprites");
-                                       imgs = sprites.getString("front_default");
-                                        pic();
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-                        }
-                    }
-                });
 
             }
         });
@@ -92,7 +78,133 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pic() {
-        Picasso.with(this).load(imgs).into(img);
+        Picasso.with(this).load(bulbasaurImageViewURL).into(bulbasaurImageView);
+        Picasso.with(this).load(squirtleImageViewURL).into(squirtleImageView);
+        Picasso.with(this).load(charmanderImageViewURL).into(charmanderImageView);
 
     }
+    public void starterPokemon(){
+        getSquirtle();
+        getCharmander();
+        getBulbasaur();
+    }
+    public void getSquirtle(){
+        final Request request = new Request.Builder()
+                .url(squirtleApiInfoURL)
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d("mode", "onFailure: ");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+                    // Log.d("mode", "onResponse: " + myResponse);
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                JSONObject obj = new JSONObject(myResponse);
+                                //JSONArray info = obj.getJSONArray("sprites");
+                                String name = obj.getString("name");
+                                JSONObject sprites = obj.getJSONObject("sprites");
+                                squirtleImageViewURL = sprites.getString("front_default");
+                                pic();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+    public void getCharmander(){
+        final Request request = new Request.Builder()
+                .url(charmanderApiInfoURL)
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d("mode", "onFailure: ");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+                    // Log.d("mode", "onResponse: " + myResponse);
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                JSONObject obj = new JSONObject(myResponse);
+                                //JSONArray info = obj.getJSONArray("sprites");
+                                String name = obj.getString("name");
+                                JSONObject sprites = obj.getJSONObject("sprites");
+                                charmanderImageViewURL = sprites.getString("front_default");
+                                pic();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    public void getBulbasaur(){
+        Log.d("mode", "onClick: ");
+        final Request request = new Request.Builder()
+                .url(bulbasaurApiInfoURL)
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d("mode", "onFailure: ");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+                    // Log.d("mode", "onResponse: " + myResponse);
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                JSONObject obj = new JSONObject(myResponse);
+                                //JSONArray info = obj.getJSONArray("sprites");
+                                String name = obj.getString("name");
+                                JSONObject sprites = obj.getJSONObject("sprites");
+                                bulbasaurImageViewURL = sprites.getString("front_default");
+                                pic();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
 }
